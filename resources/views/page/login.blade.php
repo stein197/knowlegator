ce08378536b82aa5bc375819917664cca361a969
Nail Gafarov <nil20122013@gmail.com>
2024-10-20 21:58:24 +0200

    refactor: :recycle: the view page.login uses the <x-form /> component

--- resources/views/page/login.blade.php
+++ resources/views/page/login.blade.php
@@ -5,40 +5,40 @@
 		<div class="container">
 			<div class="row">
 				<div class="col col-12 col-sm-8 col-md-6 offset-sm-2 offset-md-3">
-					<form class="card shadow" action="{{ route('login', [], false) }}" method="POST" enctype="multipart/form-data">
-						@csrf
-						<div class="card-body">
-							<p>
-								<strong>{{ __('form.login.title') }}</strong>
-							</p>
-							<div class="mb-3">
-								<p class="form-label">{{ __('form.login.field.email') }}</p>
-								<input class="form-control" type="email" name="email" required="" />
-								@error('email')
-									<p class="text-danger">{{ $message }}</p>
-								@enderror
-							</div>
-							<div class="mb-3">
-								<p class="form-label">{{ __('form.login.field.password') }}</p>
-								<input class="form-control" type="password" name="password" required="" />
-								@error('password')
-									<p class="text-danger">{{ $message }}</p>
-								@enderror
-							</div>
-							<label class="form-label">
-								<input class="from-check-input" type="checkbox" name="remember" />
-								<span>{{ __('form.login.field.remember') }}</span>
-							</label>
-							<div class="row">
-								<div class="col col-12 col-sm-6 mb-3 mb-sm-0">
-									<button class="btn btn-primary w-100" name="action" value="login">{{ __('form.login.button.login') }}</button>
-								</div>
-								<div class="col col-12 col-sm-6">
-									<button class="btn btn-success w-100" name="action" value="register">{{ __('form.login.button.register') }}</button>
-								</div>
-							</div>
-						</div>
-					</form>
+					<x-form
+						action="{{ route('login', [], false) }}"
+						method="POST"
+						title="{{ __('form.login.title') }}"
+						:fields="[
+							'email' => [
+								'label' => __('form.login.field.email'),
+								'type' => 'email',
+								'required' => true
+							],
+							'password' => [
+								'label' => __('form.login.field.password'),
+								'type' => 'password',
+								'required' => true
+							],
+							'remember' => [
+								'label' => __('form.login.field.remember'),
+								'type' => 'checkbox'
+							]
+						]"
+						:buttons="[
+							[
+								'label' => __('form.login.button.login'),
+								'type' => 'primary',
+								'name' => 'action',
+								'value' => 'login'
+							],
+							[
+								'label' => __('form.login.button.register'),
+								'type' => 'success',
+								'name' => 'action',
+								'value' => 'register'
+							]
+						]" />
 				</div>
 			</div>
 		</div>

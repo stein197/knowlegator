bd72201cf8bd9f016f5c347f6a02328ccde0099b
Nail Gafarov <nil20122013@gmail.com>
2024-11-01 16:51:46 +0100

    feat(middleware): :sparkles: add default middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,6 +9,15 @@ return Application::configure(basePath: dirname(__DIR__))
 		health: '/up',
 	)
 	->withMiddleware(function (Middleware $middleware): void {
+		$group = [
+			\Illuminate\Cookie\Middleware\EncryptCookies::class,
+			\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+			\Illuminate\Session\Middleware\StartSession::class,
+			\Illuminate\View\Middleware\ShareErrorsFromSession::class,
+			\Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+			\Illuminate\Routing\Middleware\SubstituteBindings::class
+		];
+		$middleware->group('web', $group);
 		$middleware->redirectGuestsTo(fn (): string => lroute('login'));
 		$middleware->redirectUsersTo(fn (): string => lroute('account'));
 	})

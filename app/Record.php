4fd7ad5881405d7376a0655cdbec2c616ec4fa05
Nail Gafarov <nil20122013@gmail.com>
2024-11-03 22:23:33 +0100

    feat(record): :technologist: add methods to `Record` class

--- app/Record.php
+++ app/Record.php
@@ -1,8 +1,81 @@
 <?php
 namespace App;
 
+use ReflectionClass;
+use ReflectionException;
+use ReflectionParameter;
+
+// TODO: Extract to a library
 /**
  * Base class for records.
  * @package App
  */
-abstract readonly class Record {}
+abstract readonly class Record {
+
+	/**
+	 * Check if the given argument equals to this record.
+	 * @param mixed $record Object to test against.
+	 * @return bool `true` if both records are equal.
+	 * ```php
+	 * (new Record())->equals(new Record()); // true
+	 * ```
+	 */
+	public function equals(mixed $record): bool {
+		return $record === $this || $record instanceof static && $this->toArray() === $record->toArray();
+	}
+
+	/**
+	 * Clone the record with the given properties to override.
+	 * @param array $properties Properties to override.
+	 * @return static New record.
+	 * @throws ReflectionException
+	 * ```php
+	 * (new Record(...))->with(['property' => 'value']);
+	 * ```
+	 */
+	public function with(array $properties): static {
+		return static::fromArray($properties);
+	}
+
+	/**
+	 * Convert the record to an array. The opposite of `fromArray()`.
+	 * @return array Array with the properties of a record.
+	 * ```php
+	 * (new Record(a: 'first'))->toArray(); // ['a' => 'first']
+	 * ```
+	 */
+	public function toArray(): array {
+		$parameters = static::parameters();
+		$result = [];
+		foreach ($parameters as $p)
+			$result[$p->name] = $this->{$p->name};
+		return $result;
+	}
+
+	/**
+	 * Convert an array to a record. The opposite of `toArray()`.
+	 * @param array $properties Properties that the record should have. Properties without values will have a default value.
+	 * @return static Record
+	 * @throws ReflectionException
+	 * ```php
+	 * Record::fromArray(['a' => 'first']); // Record {a: 'first'}
+	 * ```
+	 */
+	public static function fromArray(array $properties): static {
+		$parameters = static::parameters();
+		$result = [];
+		foreach ($parameters as $p)
+			$result[] = isset($properties[$p->name]) ? $properties[$p->name] : $p->getDefaultValue();
+		return new static(...$result);
+	}
+
+	/**
+	 * @return ReflectionParameter[]
+	 */
+	private static function parameters(): array {
+		static $cache = [];
+		if (!$cache)
+			$cache = (new ReflectionClass(static::class))->getConstructor()->getParameters();
+		return $cache;
+	}
+}
---
+++ tests/Unit/RecordTest.php
@@ -0,0 +1,73 @@
+<?php
+namespace Tests\Unit;
+
+use App\Record;
+
+describe('equals()', function (): void {
+	test('should return false for null', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->assertFalse((new TestRecord())->equals(null));
+	});
+
+	test('should return false for another instance', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->assertFalse((new TestRecord)->equals(new \stdClass));
+	});
+
+	test('should return false when values are different', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->assertFalse((new TestRecord(a: 'first', b: 'second'))->equals(new TestRecord(a: 'first')));
+	});
+
+	test('should return true when values are the same', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->assertTrue((new TestRecord(a: 'first'))->equals(new TestRecord(a: 'first')));
+	});
+});
+
+describe('with()', function (): void {
+	test('should return clone when the array is empty', function (): void {
+		/** @var \Tests\TestCase $this */
+		$r = new TestRecord;
+		$this->assertTrue($r->equals($r->with([])));
+	});
+
+	test('should override only one parameters', function (): void {
+		/** @var \Tests\TestCase $this */
+		$r = new TestRecord;
+		$this->assertTrue((new TestRecord(a: 'first'))->equals($r->with(['a' => 'first'])));
+	});
+
+	test('should override all parameters', function (): void {
+		/** @var \Tests\TestCase $this */
+		$r = new TestRecord;
+		$this->assertTrue((new TestRecord(a: 'first', b: 'second'))->equals($r->with(['a' => 'first', 'b' => 'second'])));
+	});
+});
+
+describe('toArray()', function (): void {
+	test('should return an array', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->assertEquals(['a' => 'first', 'b' => 'second'], (new TestRecord(a: 'first', b: 'second'))->toArray());
+	});
+});
+
+describe('fromArray()', function (): void {
+	test('should return an instance when not all properties are defined', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->assertTrue((new TestRecord())->equals(TestRecord::fromArray([])));
+		$this->assertTrue((new TestRecord(a: 'first'))->equals(TestRecord::fromArray(['a' => 'first'])));
+	});
+
+	test('should return an instance when all properties are defined', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->assertTrue((new TestRecord(a: 'first', b: 'second'))->equals(TestRecord::fromArray(['a' => 'first', 'b' => 'second'])));
+	});
+});
+
+final readonly class TestRecord extends Record {
+	public function __construct(
+		public string $a = 'a',
+		public string $b = 'b'
+	) {}
+}

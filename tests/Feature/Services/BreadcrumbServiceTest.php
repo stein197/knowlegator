f993d79dc2d703d1505d6a142639b3153b62a531
Nail Gafarov <nil20122013@gmail.com>
2024-11-06 22:09:29 +0100

    fix(test): :white_check_mark: fix `Tests\Feature\Services\BreadcrumbServiceTest`

--- tests/Feature/Services/BreadcrumbServiceTest.php
+++ tests/Feature/Services/BreadcrumbServiceTest.php
@@ -6,14 +6,13 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 uses(DatabaseTransactions::class);
 
-//FIXME: XPath does not work with <script /> and <style />
 test('GET /en/settings/password', function (): void {
 	/** @var \Tests\TestCase $this */
 	$u = User::factory()->create();
 	$content = $this->actingAs($u)->get('/en/settings/password')->getContent();
 	$dom = $this->dom($content);
 	$dom->find('//ol[@class="breadcrumb"]/li[position() = 1]')->assertTextContent('Settings');
-	$dom->find('//ol[@class="breadcrumb"]/li[position() = 2]')->assertTextContent('Change Password');
+	$dom->find('//ol[@class="breadcrumb"]/li[position() = 2]')->assertTextContent('Change password');
 });
 
 test('GET /en/account/tags', function (): void {
@@ -22,7 +21,7 @@ test('GET /en/account/tags', function (): void {
 	$content = $this->actingAs($u)->get('/en/account/tags')->getContent();
 	$dom = $this->dom($content);
 	$dom->find('//ol[@class="breadcrumb"]/li[position() = 1]')->assertTextContent('Account');
-	$dom->find('//ol[@class="breadcrumb"]/li[position() = 2]')->assertTextContent('Tags');
+	$dom->find('//ol[@class="breadcrumb"]/li[position() = 2]/a')->assertTextContent('Tags');
 });
 
 test('GET /en/account/tags/create', function (): void {
@@ -31,7 +30,7 @@ test('GET /en/account/tags/create', function (): void {
 	$content = $this->actingAs($u)->get('/en/account/tags/create')->getContent();
 	$dom = $this->dom($content);
 	$dom->find('//ol[@class="breadcrumb"]/li[position() = 1]')->assertTextContent('Account');
-	$dom->find('//ol[@class="breadcrumb"]/li[position() = 2]')->assertTextContent('Tags');
+	$dom->find('//ol[@class="breadcrumb"]/li[position() = 2]/a')->assertTextContent('Tags');
 	$dom->assertExists('//ol[@class="breadcrumb"]/li[position() = 2]/a[@href = "/en/account/tags"]');
-	$dom->find('//ol[@class="breadcrumb"]/li[position() = 3]')->assertTextContent('New Tag');
+	$dom->find('//ol[@class="breadcrumb"]/li[position() = 3]')->assertTextContent('New tag');
 });

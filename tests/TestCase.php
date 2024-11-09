a2f6e967f71921df3a4988531c22233a4be6da2f
Nail Gafarov <nil20122013@gmail.com>
2024-11-09 23:05:39 +0100

    feat(test): :technologist: add `domComponent()` method to `Tests\TestCase`

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,7 @@
 <?php
 namespace Tests;
 
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 // TODO: Use test database for testing
@@ -14,5 +15,19 @@ abstract class TestCase extends BaseTestCase {
 	 */
 	public function dom(string $dom): TestDOM {
 		return new TestDOM([$dom], $this);
+	}
+
+	/**
+	 * Render Blade component and return DOM-object to assert XPath against.
+	 * @param string $component Component class name.
+	 * @param array $data Optional component parameters.
+	 * @return TestDOM
+	 * @throws BindingResolutionException
+	 * ```php
+	 * $this->domComponent(Alert::class, [])->assertExists('//p[@class = "alert"]');
+	 * ```
+	 */
+	public function domComponent(string $component, array $data = []): TestDOM {
+		return $this->dom((string) $this->component($component, $data)->component->render());
 	}
 }

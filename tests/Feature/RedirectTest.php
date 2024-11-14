c4c67050c7302fc5f328e56c8bca4034cec7da07
Nail Gafarov <nil20122013@gmail.com>
2024-11-14 19:26:25 +0100

    refactor(test): :recycle: refactor RedirectTest

--- tests/Feature/RedirectTest.php
+++ tests/Feature/RedirectTest.php
@@ -2,48 +2,65 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-uses(DatabaseTransactions::class);
-
-test('GET /{locale}/account should redirect to /{locale}/account/entities for a user', function () {
-	/** @var \Tests\TestCase $this */
-	$this->actingAs(User::findByEmail('user-1@example.com'));
-	$this->get('/en/account')->assertRedirect('/en/account/entities');
-});
-
-test('GET /{locale}/settings should redirect to /{locale}/settings/password for a user', function (): void {
-	/** @var \Tests\TestCase $this */
-	$response = $this->actingAs(User::findByEmail('user-1@example.com'))->get('/en/settings');
-	$response->assertRedirect('/en/settings/password');
-});
-
-describe('Redirect to /{locale}/login for guests', function (): void {
-	test('should redirect when accessing "/" and the user', function (): void {
+describe('GET /', function (): void {
+	test('-> /{locale}/login for guests', function (): void {
+		/** @var \Tests\TestCase $this */
 		$this->get('/')->assertRedirect('/en/login');
 	});
+	test('-> /{locale}/account/entities for users', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->actingAs(User::findByEmail('user-1@example.com'))->get('/')->assertRedirect('/en/account/entities');
+	});
+});
 
-	test('should redirect when accessing "/en"', function (): void {
+describe('GET /{locale}', function (): void {
+	test('-> /{locale}/login for guests', function (): void {
+		/** @var \Tests\TestCase $this */
 		$this->get('/en')->assertRedirect('/en/login');
 	});
 
-	test('should redirect when accessing "/en/account"', function (): void {
-		$this->get('/en/account')->assertRedirect('/en/login');
+	test('-> /{locale}/account/entities for users', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->actingAs(User::findByEmail('user-1@example.com'))->get('/en')->assertRedirect('/en/account/entities');
 	});
 });
 
-describe('Redirect to /{locale}/account/entities for users', function (): void {
-	test('should be redirected when accessing "/"', function (): void {
-		$this->actingAs(User::findByEmail('user-1@example.com'))->get('/')->assertRedirect('/en/account/entities');
+describe('GET /{locale}/account', function (): void {
+	test('-> /{locale}/account/entities for users', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->actingAs(User::findByEmail('user-1@example.com'));
+		$this->get('/en/account')->assertRedirect('/en/account/entities');
+		$this->get('/de/account')->assertRedirect('/de/account/entities');
 	});
 
-	test('should be redirected when accessing "/en"', function (): void {
-		$this->actingAs(User::findByEmail('user-1@example.com'))->get('/en')->assertRedirect('/en/account/entities');
+	test('-> /{locale}/login for guests', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->get('/en/account')->assertRedirect('/en/login');
+		$this->get('/de/account')->assertRedirect('/de/login');
+	});
+});
+
+describe('GET /{locale}/settings', function (): void {
+	test('-> /{locale}/settings/password for users', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->actingAs(User::findByEmail('user-1@example.com'));
+		$this->get('/en/settings')->assertRedirect('/en/settings/password');
+		$this->get('/de/settings')->assertRedirect('/de/settings/password');
 	});
 
-	test('should be redirected when accessing "/en/login"', function (): void {
-		$this->actingAs(User::findByEmail('user-1@example.com'))->get('/en/login')->assertRedirect('/en/account');
+	test('-> /{locale}/login for guests', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->get('/en/settings')->assertRedirect('/en/login');
+		$this->get('/de/settings')->assertRedirect('/de/login');
 	});
+});
+
+test('GET /{locale}/login -> /{locale}/account for users', function (): void {
+	/** @var \Tests\TestCase $this */
+	$this->actingAs(User::findByEmail('user-1@example.com'));
+	$this->get('/en/login')->assertRedirect('/en/account');
+	$this->get('/de/login')->assertRedirect('/de/account');
 });
 
 test('should redirect to "/<route>" when accessing "/<route>/"', function (): void {

58097dd069e0c30147898fe8b950176a5efb5cfe
Nail Gafarov <nil20122013@gmail.com>
2024-11-16 22:51:50 +0100

    feat(function): :technologist: add `App\{array_entries, array_unset}()` functions

--- app/util.php
+++ app/util.php
@@ -3,6 +3,36 @@
 namespace App;
 
 /**
+ * Return array entries.
+ * @param array $array Array to return entries from.
+ * @return array Array of entries, the first element is key, the second is value.
+ * ```php
+ * array_entries(['a' => 1]); // [['a', 1]]
+ * ```
+ */
+function array_entries(array $array): array {
+	$result = [];
+	foreach ($array as $k => $v)
+		$result[] = [$k, $v];
+	return $result;
+}
+
+/**
+ * Unset array keys.
+ * @param array $array Array to unset keys from.
+ * @param array $keys Keys to unset.
+ * @return array Array with unset keys.
+ * ```php
+ * array_unset(['a' => 1, 'b' => 2], ['b']); // ['a' => 1]
+ * ```
+ */
+function array_unset(array $array, array $keys): array {
+	foreach ($keys as $k)
+		unset($array[$k]);
+	return $array;
+}
+
+/**
  * Split path into an array of segments
  * @param string $path Path to split
  * @return string[] Segments
--- tests/Unit/UtilTest.php
+++ tests/Unit/UtilTest.php
@@ -1,7 +1,35 @@
 <?php
 namespace Tests\Util;
 
-use function App\path_split;
+use function App\{
+	array_entries,
+	array_unset,
+	path_split,
+};
+
+describe('array_entries()', function (): void {
+	test('should return an empty array when the array is empty', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->assertSame([], array_entries([]));
+	});
+
+	test('should return array entries', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->assertSame([['a', 1]], array_entries(['a' => 1]));
+	});
+});
+
+describe('array_unset()', function (): void {
+	test('should unset provided keys', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->assertSame(['a' => 1], array_unset(['a' => 1, 'b' => 2], ['b']));
+	});
+
+	test('should do nothing when the provided keys don\'t exist', function (): void {
+		/** @var \Tests\TestCase $this */
+		$this->assertSame(['a' => 1], array_unset(['a' => 1], ['b']));
+	});
+});
 
 describe('path_split()', function (): void {
 	test('should return an empty array when the path is root', function (): void {

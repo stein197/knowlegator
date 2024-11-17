62c73fb3519c3dc5653eec67599d207f44b9c8e2
Nail Gafarov <nil20122013@gmail.com>
2024-11-17 18:15:10 +0100

    feat(model): :technologist: add `App\Model->getActionUrl()` method

--- app/Model.php
+++ app/Model.php
@@ -2,8 +2,13 @@
 namespace App;
 
 use App\Models\User;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Illuminate\Support\Pluralizer;
+use InvalidArgumentException;
+use ReflectionClass;
+use function strtolower;
 
 /**
  * Base model class. Provices a convenient way to set `user_id` property.
@@ -29,10 +34,32 @@ abstract class Model extends EloquentModel {
 	}
 
 	/**
+	 * Return absolute URL link to this entity based on resource controllers.
+	 * @param string $action Resource action, like 'show', 'edit'
+	 * @return string URL to the action of the resource.
+	 * @throws InvalidArgumentException
+	 * @throws BindingResolutionException
+	 * ```php
+	 * $t = new Tag();
+	 * $t->getActionUrl('edit'); // "/en/account/tags/<uuid>/edit"
+	 * ```
+	 */
+	final public function getActionUrl(string $action): string {
+		[$singular, $plural] = static::getModelTypeNames();
+		return lroute("{$plural}.{$action}", [$singular => $this->id]);
+	}
+
+	/**
 	 * Return names of publicly available attributes that can be edited or be shown.
 	 * @return string[] Publicly available attributes.
 	 */
 	public static function getPublicAttributes(): array {
 		return ['name'];
+	}
+
+	private static function getModelTypeNames(): array {
+		$class = new ReflectionClass(static::class);
+		$name = strtolower($class->getShortName());
+		return [$name, Pluralizer::plural($name)];
 	}
 }

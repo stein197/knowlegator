e5547c79fe4304563b62618d54b9702ff64d7c68
Nail Gafarov <nil20122013@gmail.com>
2024-11-17 18:15:48 +0100

    refactor(view): :lipstick: add show, edit and delete links to `etypes.index` route

--- resources/views/resource/etype/index.blade.php
+++ resources/views/resource/etype/index.blade.php
@@ -1,9 +1,20 @@
 @extends('template.resource.index')
 
 @section('data')
-	<div class="list-group">
+	<ul class="list-group">
 		@foreach ($data as $etype)
-			<a class="list-group-item list-group-item-action" href="{{ lroute('etypes.show', ['etype' => $etype->id]) }}">{{ $etype->name }}</a>
+			<li class="list-group-item d-flex" href="{{ lroute('etypes.show', ['etype' => $etype->id]) }}">
+				<a class="flex-grow-1 text-decoration-none" href="{{ $etype->getActionUrl('show') }}">{{ $etype->name }}</a>
+				<a class="btn reset text-decoration-none mx-2" data-bs-toggle="tooltip" title="{{ __('action.show') }}" href="{{ $etype->getActionUrl('show') }}">
+					<i class="bi bi-eye-fill"></i>
+				</a>
+				<a class="btn reset text-decoration-none mx-2" data-bs-toggle="tooltip" title="{{ __('action.edit') }}" href="{{ $etype->getActionUrl('edit') }}">
+					<i class="bi bi-pen-fill"></i>
+				</a>
+				<a class="btn reset text-decoration-none mx-2" data-bs-toggle="tooltip" title="{{ __('action.delete') }}" href="{{ $etype->getActionUrl('delete') }}">
+					<i class="bi bi-trash-fill"></i>
+				</a>
+			</li>
 		@endforeach
-	</div>
+	</ul>
 @endsection

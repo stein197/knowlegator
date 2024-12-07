52dd7bf3199434fc61efbf58df5e29ffdbaf547a
Nail Gafarov <nil20122013@gmail.com>
2024-12-07 22:13:10 +0100

    revert(blade): :rewind: `resource.show`

--- resources/views/template/resource/show.blade.php
+++ resources/views/template/resource/show.blade.php
@@ -1,7 +1,23 @@
 @extends('template.menu')
 
 @section('content')
-	{{ $form->view() }}
+	<table class="table">
+		<caption class="caption-top">{{ __('table.resource.caption') }}</caption>
+		<thead>
+			<tr>
+				<th>{{ __('table.resource.col.key') }}</th>
+				<th>{{ __('table.resource.col.value') }}</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach ($model::getPublicAttributes() as $k => $Field)
+				<tr>
+					<td>{{ $k }}</td>
+					<td>{{ $model->{$k} }}</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
 	<div class="btn-group">
 		@foreach ($buttons as $btn)
 			<a class="btn btn-outline-secondary" href="{{ $btn->url }}" data-bs-toggle="tooltip" data-bs-title="{{ $btn->label }}">

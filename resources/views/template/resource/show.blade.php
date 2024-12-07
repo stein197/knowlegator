166d227f04a8525dd45f8d3d943dfa1167d7969b
Nail Gafarov <nil20122013@gmail.com>
2024-12-07 22:10:22 +0100

    style(form): :recycle: properties are private

--- resources/views/template/resource/show.blade.php
+++ resources/views/template/resource/show.blade.php
@@ -1,23 +1,7 @@
 @extends('template.menu')
 
 @section('content')
-	<table class="table">
-		<caption class="caption-top">{{ __('table.resource.caption') }}</caption>
-		<thead>
-			<tr>
-				<th>{{ __('table.resource.col.key') }}</th>
-				<th>{{ __('table.resource.col.value') }}</th>
-			</tr>
-		</thead>
-		<tbody>
-			@foreach ($model::getPublicAttributes() as $k => $Field)
-				<tr>
-					<td>{{ $k }}</td>
-					<td>{{ $model->{$k} }}</td>
-				</tr>
-			@endforeach
-		</tbody>
-	</table>
+	{{ $form->view() }}
 	<div class="btn-group">
 		@foreach ($buttons as $btn)
 			<a class="btn btn-outline-secondary" href="{{ $btn->url }}" data-bs-toggle="tooltip" data-bs-title="{{ $btn->label }}">

a30e7d397339c9168b05fee44711d2d6eb524ae7
Nail Gafarov <nil20122013@gmail.com>
2024-12-07 21:58:31 +0100

    style(blade): :fire: delete `page.message` template

--- resources/views/page/message.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('template.content')
-
-@section('content')
-	<x-alert type="{{ $type }}">{{ $message }}</x-alert>
-	<a class="btn btn-dark" href="{{ url()->previous() }}">{{ __('action.back') }}</a>
-@endsection

bc6b66776501fe75f647d9cdb4a9d9e21283c76b
Nail Gafarov <nil20122013@gmail.com>
2024-12-14 19:21:26 +0100

    feat(route): :technologist: add classname support for the `App\Services\ModelRoutingService->route()`

--- app/Services/ModelRoutingService.php
+++ app/Services/ModelRoutingService.php
@@ -12,19 +12,22 @@ use Illuminate\Support\Pluralizer;
 final readonly class ModelRoutingService {
 
 	/**
-	 * Generate an URL for the given model and action.
-	 * @param Model $model Model to generate an URL for.
+	 * Generate an URL for the given model or model class and action.
+	 * @param string|Model $model Model or a model class to generate an URL for.
 	 * @param string $action Action the URL pointing to.
 	 * @return ?string Generated URL or `null` if the route doesn't exist.
 	 * ```php
-	 * $this->route($tagModel, 'edit'); // '/en/tags/<id>/edit'
+	 * $this->route($tagModel, 'edit');   // '/en/account/tags/<id>/edit'
+	 * $this->route(Tag::class, 'index'); // '/en/account/tags'
 	 * ```
 	 */
-	public function route(Model $model, string $action): ?string {
-		$tName = $model::class::getTypeName();
+	public function route(string | Model $model, string $action): ?string {
+		$isModel = $model instanceof Model;
+		$class = $isModel ? $model::class : $model;
+		$tName = $class::getTypeName();
 		$plural = Pluralizer::plural($tName);
 		try {
-			return lroute("{$plural}.{$action}", [$tName => $model->id]);
+			return lroute("{$plural}.{$action}", $isModel ? [$tName => $model->id] : []);
 		} catch (Exception) {
 			return null;
 		}
--- tests/Feature/Services/ModelRoutingServiceTest.php
+++ tests/Feature/Services/ModelRoutingServiceTest.php
@@ -1,21 +1,38 @@
 <?php
 namespace Tests\Feature\Services;
 
+use App\Models\Tag;
 use App\Models\User;
 use App\Services\ModelRoutingService;
 
 describe('route()', function (): void {
-	test('should return url when the action exists', function (): void {
-		/** @var \Tests\TestCase $this */
-		$t = User::findByEmail('user-1@example.com')->tags[0];
-		$service = $this->createApplication()->make(ModelRoutingService::class);
-		$this->assertSame("/en/account/tags/{$t->id}/edit", $service->route($t, 'edit'));
+
+	describe('instance', function (): void {
+		test('should return url when the action exists', function (): void {
+			/** @var \Tests\TestCase $this */
+			$t = User::findByEmail('user-1@example.com')->tags[0];
+			$service = $this->createApplication()->make(ModelRoutingService::class);
+			$this->assertSame("/en/account/tags/{$t->id}/edit", $service->route($t, 'edit'));
+		});
+
+		test('should return null when the action does not exist', function (): void {
+			/** @var \Tests\TestCase $this */
+			$t = User::findByEmail('user-1@example.com')->tags[0];
+			$service = $this->createApplication()->make(ModelRoutingService::class);
+			$this->assertNull($service->route($t, 'unknown action'));
+		});
 	});
 
-	test('should return null when the action does not exist', function (): void {
-		/** @var \Tests\TestCase */
-		$t = User::findByEmail('user-1@example.com')->tags[0];
-		$service = $this->createApplication()->make(ModelRoutingService::class);
-		$this->assertNull($service->route($t, 'unknown action'));
+	describe('class', function (): void {
+		test('should return url when the action exists', function (): void {
+			/** @var \Tests\TestCase $this */
+			$service = $this->createApplication()->make(ModelRoutingService::class);
+			$this->assertSame('/en/account/tags', $service->route(Tag::class, 'index'));
+		});
+		test('should return null when the action does not exist', function (): void {
+			/** @var \Tests\TestCase $this */
+			$service = $this->createApplication()->make(ModelRoutingService::class);
+			$this->assertNull($service->route(Tag::class, 'unknown action'));
+		});
 	});
 });
